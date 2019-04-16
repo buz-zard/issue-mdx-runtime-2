96d726da687365188e64af9798483b9244f61f4a
Karolis Šarapnickis <pastas.k@gmail.com>
2019-04-16 23:58:35 +0300

    Update version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import MDX from '@mdx-js/runtime';
 
-const mdx = `
-# Hello
+// Provide custom components for markdown elements
+const components = {
+  // eslint-disable-next-line jsx-a11y/heading-has-content
+  h1: props => <h1 style={{ color: 'tomato' }} {...props} />,
+};
 
-<div children="OK" />
+// Provide custom components that will be referenced as JSX
+// in the markdown string
+const scope = {
+  Demo: () => <h1>This is a demo component</h1>,
+};
+
+const mdx = `
+# Hello, world!
+
+<Demo />
 `;
 
-export default function App() {
-  return (
-    <div>
-      <MDX>{mdx}</MDX>
-    </div>
-  );
-}
+export default () => (
+  <MDX components={components} scope={scope}>
+    {mdx}
+  </MDX>
+);
